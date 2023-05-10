fcaeb73fc7fea19a9ebc51c3f153e823af6f6a52
Shivam Chourasia <shivamchourasia2212@gmail.com>
2023-05-11 00:25:26 +0530

    everything corrected

--- sampleJSQuiz/main.js
+++ sampleJSQuiz/main.js
@@ -1,10 +1,10 @@
 let AllQuestions = [];
 fetch("./allQuestion.json")
   .then((res) => res.json())
-  .then((data)=>{
-    AllQuestions = data
+  .then((data) => {
+    AllQuestions = data;
     printQuestion(AllQuestions);
-  })
+  });
 
 /// const AllQuestions = [...AllQuestion];
 
@@ -28,25 +28,33 @@ function printQuestion(AllQuestions) {
 
     <div class="form-check">
     <input class="form-check-input" type="radio" name="question${i}"  
-    id="${i + "01"}" value="${AllQuestions[i].a}" >
+    id="${i + "01"}" value="${AllQuestions[i].choice1}" >
     <label class="form-check-label" for="${i + "01"}">
-    ${AllQuestions[i].a}
+    ${AllQuestions[i].choice1}
     </label>
   </div>
 
   <div class="form-check  ">
     <input class="form-check-input" type="radio" name="question${i}"  
-    id="${i + "02"}" value="${AllQuestions[i].b}" >
+    id="${i + "02"}" value="${AllQuestions[i].choice2}" >
     <label class="form-check-label " for="${i + "02"}" >
-    ${AllQuestions[i].b}
+    ${AllQuestions[i].choice2}
     </label>
   </div>
 
   <div class="form-check">
   <input class="form-check-input" type="radio"  name="question${i}"  
-  id="${i + "03"}" value="${AllQuestions[i].c}" >
+  id="${i + "03"}" value="${AllQuestions[i].choice3}" >
   <label class="form-check-label" for="${i + "03"}" >
-  ${AllQuestions[i].c}
+  ${AllQuestions[i].choice3}
+  </label>
+  </div>
+
+  <div class="form-check">
+  <input class="form-check-input" type="radio"  name="question${i}"  
+  id="${i + "03"}" value="${AllQuestions[i].choice4}" >
+  <label class="form-check-label" for="${i + "03"}" >
+  ${AllQuestions[i].choice4}
   </label>
   </div>
 </div>
@@ -80,19 +88,22 @@ function checkAnswers() {
 
     for (var j = 0; j < EachQuestion.length; j++) {
       if (EachQuestion[j].checked) {
-        if (EachQuestion[j].value == AllQuestions[i].correctAnswer) {
+        if (EachQuestion[j].id.slice(-1) == AllQuestions[i].answer) {
+          console.log(3 == EachQuestion[j].id);
           EachQuestion[j].style.backgroundColor = "Green";
           score += 1;
           EachQuestion[j].checked = false;
         } else {
           EachQuestion[j].style.backgroundColor = "red";
+          console.log(EachQuestion[j].id);
           EachQuestion[j].checked = false;
           wrong.innerHTML += `<strong>Question ${
             i + 1
           } you got it wrong!</strong> <br>`;
         }
       } else {
-        if (EachQuestion[j].value == AllQuestions[i].correctAnswer) {
+        if (EachQuestion[j].id == AllQuestions[i].answer) {
+          console.log(EachQuestion[j].id);
           EachQuestion[j].style.backgroundColor = "Green";
         }
         EachQuestion[j].disabled = true;
